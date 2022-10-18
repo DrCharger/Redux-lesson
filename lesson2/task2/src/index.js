4248f4128a2321cbefad6cd9342ac92520af70b9
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-10-18 16:12:56 +0300

    utd

--- lesson2/task2/src/index.js
+++ lesson2/task2/src/index.js
@@ -23,6 +23,6 @@ resetBTN.addEventListener('click', onReset);
 store.subscribe(() => {
   const state = store.getState();
   const currentValue = state.history.reduce((acc, elem) => acc + Number(elem), 0);
-  const historyString = state.history.reduce((acc, elem) => acc + elem, 0);
+  const historyString = state.history.join('');
   counter.textContent = state.history.length === 0 ? '' : `${historyString} = ${currentValue}`;
 });
