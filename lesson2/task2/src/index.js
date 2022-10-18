10b2d981790c47637cbae42de6d90ffab2316f67
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-10-18 16:08:49 +0300

    jutrtfg

---
+++ lesson2/task2/src/index.js
@@ -0,0 +1,28 @@
+import store, { increment, decrement, reset } from './store';
+import './index.scss';
+
+const counter = document.querySelector('.counter__result');
+const incBTN = document.querySelector('[data-action = "increment"]');
+const decBTN = document.querySelector('[data-action = "decrement"]');
+const resetBTN = document.querySelector('[data-action = "reset"]');
+
+const onIncrement = () => {
+  store.dispatch(increment());
+};
+const onDecrement = () => {
+  store.dispatch(decrement());
+};
+const onReset = () => {
+  store.dispatch(reset());
+};
+
+incBTN.addEventListener('click', onIncrement);
+decBTN.addEventListener('click', onDecrement);
+resetBTN.addEventListener('click', onReset);
+
+store.subscribe(() => {
+  const state = store.getState();
+  const currentValue = state.history.reduce((acc, elem) => acc + Number(elem), 0);
+  const historyString = state.history.reduce((acc, elem) => acc + elem, 0);
+  counter.textContent = state.history.length === 0 ? '0' : `${historyString} = ${currentValue}`;
+});
