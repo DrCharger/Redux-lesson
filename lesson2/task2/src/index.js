7e30ebc48a9a2a2e4675bf61a42bfa52d7d2fbfd
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-10-18 19:41:19 +0300

    sdfsdf

--- lesson2/task2/src/index.js
+++ lesson2/task2/src/index.js
@@ -2,9 +2,9 @@ import store, { increment, decrement, reset } from './store';
 import './index.scss';
 
 const counter = document.querySelector('.counter__result');
-const incBtn = document.querySelector('[data-action = "increment"]');
-const decBtn = document.querySelector('[data-action = "decrement"]');
-const resetBtn = document.querySelector('[data-action = "reset"]');
+const incBtn = document.querySelector('[data-action="increment"]');
+const decBtn = document.querySelector('[data-action="decrement"]');
+const resetBtn = document.querySelector('[data-action="reset"]');
 
 const onIncrement = () => {
   store.dispatch(increment());
