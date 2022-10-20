261a37c76fdd14b108aa897a4815744858514cd6
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-10-20 14:02:47 +0300

    jhf

---
+++ lesson3/task2/src/index.js
@@ -0,0 +1,15 @@
+import store, { increment, decrement } from './counter';
+
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(decrement());
