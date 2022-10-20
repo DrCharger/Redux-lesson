213863b081fae6fcbac3ea264b5dd4ae4f8dc7fd
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-10-20 14:11:45 +0300

    mg

--- lesson3/task2/src/counter.js
+++ lesson3/task2/src/counter.js
@@ -1,34 +1,32 @@
 import { createStore } from 'redux';
 
-// const INCREMENT = 'COUNTER/INCREMENT';
-// const DECREMENT = 'COUNTER/DECREMENT';
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
 
 export const increment = () => {
   return {
-    type: 'COUNTER/INCREMENT',
+    type: INCREMENT,
   };
 };
 
 export const decrement = () => {
   return {
-    type: 'COUNTER/DECREMENT',
+    type: DECREMENT,
   };
 };
 
 const counterReducer = (state = 0, action) => {
   switch (action.type) {
-    case 'COUNTER/INCREMENT':
+    case INCREMENT:
       return state + 1;
-    case 'COUNTER/DECREMENT':
+    case DECREMENT:
       return state - 1;
     default:
       return state;
   }
 };
 
-const store = createStore(
+export const store = createStore(
   counterReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
-
-export default store;
