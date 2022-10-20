4b794a86fc8d9b29b501bfa0d82f5e2401cb7e95
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-10-20 15:37:24 +0300

    er

--- lesson3/task3/src/user.reducer.js
+++ lesson3/task3/src/user.reducer.js
@@ -3,7 +3,7 @@ import { ADD_USER, DELETE_USER } from './user.actions';
 const userReducer = (state = null, action) => {
   switch (action.type) {
     case ADD_USER:
-      return action.payload.userData.name;
+      return action.payload.userData;
     case DELETE_USER:
       return null;
     default:
