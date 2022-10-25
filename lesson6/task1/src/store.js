4e3a611a46c09365cbf75c6e3465e0128c51e14c
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-10-25 17:16:40 +0300

    hgfd

---
+++ lesson6/task1/src/store.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import counterReducer from './counter/counter.reducers';
+
+const appReducer = combineReducers({
+  counter: counterReducer,
+});
+
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  const result = next(action);
+  console.log('new state', store.getState());
+  console.groupEnd();
+  return result;
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSIONS_COMPOSE__ || compose;
+
+const store = createStore(appReducer, composeEnhancers(applyMiddleware(logger)));
+
+export default store;
